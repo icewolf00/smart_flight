913c0d6e183a760dd7af4d98625fcb19b3774888
wuling31715 <wuling31715@gmail.com>
2017-10-08 17:48:11 +0800

    u

--- bot/views.py
+++ bot/views.py
@@ -37,11 +37,13 @@ class Main:
         company_name = ''
         for row in flight[0][1:]:
             data = data + row
+            '''
             company_name_start = row.index('航空公司')
             company_name_end = row.index('出發時間')
             company_name = row[company_name_start:company_name_end] 
             print('asnclknsalckn' + company_name)
-        return data, city_url, company_name   
+            '''
+        return data, city_url   
 
 
 #main = Main 
